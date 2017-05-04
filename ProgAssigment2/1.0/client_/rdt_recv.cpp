bad2156fa8dd5828b0a0ec3258b00dbdc5a82f83
aurelioarango <arango.aurelio@hotmail.com>
2017-05-04 13:10:24 -0700

    changed send and rcv

--- ProgAssigment2/1.0/client_/rdt_recv.cpp
+++ ProgAssigment2/1.0/client_/rdt_recv.cpp
@@ -7,19 +7,18 @@
 #include <stdlib.h>
 #include <cstring>
 #include <sys/socket.h>
-//#include "packet.h"
 using namespace std;
 
 extern unsigned short cksum(unsigned char *addr, int nbytes);
 struct packet {
-  public:
         uint16_t cksum; /* Ack and Data */
         uint16_t len;   /* Ack and Data */
         uint32_t ackno; /* Ack and Data */
         uint32_t seqno; /* Data only */
         char data[500]; /* Data only; Not always 500 bytes, can be less */
-} ;
+}  ;
 
+//typedef struct packet packet_t;
 
 int rdt_recv(int socket_descriptor,
        	     char *buffer,
@@ -29,12 +28,17 @@ int rdt_recv(int socket_descriptor,
              int  address_length)
 {
   //Keep reading data while n > 0
-
-  packet packet_t;
-  //typedef struct packet packet_t;
+   packet packet_t;
   int checksum=1;//to get into loop
   int cal_checksum =0;//calculate a new checksum with the new data
-  while(checksum != cal_checksum)
+
+  recvfrom(socket_descriptor, (void *)&packet_t, sizeof(packet_t), flags,
+    (struct sockaddr *)&from_address,(socklen_t *) &address_length);
+
+    memcpy ( buffer, packet_t.data, sizeof(packet_t.data) );
+    checksum=packet_t.cksum;
+    cal_checksum = cksum((unsigned char *)buffer, strlen(buffer));
+/*  while(checksum != cal_checksum)
   {
 
     //get data from the other side
@@ -48,8 +52,8 @@ int rdt_recv(int socket_descriptor,
     checksum=packet_t.cksum;
     //calculate check sum
     cal_checksum = cksum((unsigned char *)buffer, sizeof(buffer));
-  }
-
+  }*/
+  cout <<"Data: " <<buffer << " sum "<< checksum<<" new cksum " << cal_checksum<<endl;
 
   return 0;
 }
--- ProgAssigment2/1.0/server_/rdt_send.cpp
+++ ProgAssigment2/1.0/server_/rdt_send.cpp
@@ -10,15 +10,16 @@ using namespace std;
 //use error handling
 extern int error(const char * format,...);
 extern unsigned short cksum(unsigned char *addr, int nbytes);
+
 struct packet {
         uint16_t cksum; /* Ack and Data */
         uint16_t len;   /* Ack and Data */
         uint32_t ackno; /* Ack and Data */
         uint32_t seqno; /* Data only */
         char data[500]; /* Data only; Not always 500 bytes, can be less */
-};
+} ;
 //defining the struct
-//typedef struct packet packet_l;
+//typedef struct packet packet_t;
 
 int rdt_sendto(int socket_descriptor,
               char *buffer,
@@ -26,13 +27,15 @@ int rdt_sendto(int socket_descriptor,
               int flags,
               struct sockaddr *destination_address,
               int address_length){
+
   packet packet_t;
   int ck_sum =cksum((unsigned char *)buffer,strlen(buffer));
   packet_t.seqno = 0;
   packet_t.ackno = 0;
-  unsigned int len = address_length;
+  unsigned int len = buffer_length;
+  //cout << buffer << " len " <<len << " "<<buffer_length <<endl;
   //Break data into packets and send it over UDP use a foorloop to keep sending the data.
-   while ( len > sizeof(packet_t.data) )
+  /* while ( len > sizeof(packet_t.data) )
    {
 
      packet_t.len = sizeof(packet_t.data);
@@ -49,23 +52,32 @@ int rdt_sendto(int socket_descriptor,
 
      //send the data
      if (sendto(socket_descriptor, (void *)& packet_t, sizeof(packet_t), flags, (struct sockaddr *)&destination_address, address_length) == -1) {
-         cout << "Error sending to client, errno = %d (%s) \n" <<endl;
+          error("Error sending to client, errno \n");
          return -1;
      }
      packet_t.seqno++;
      packet_t.ackno++;
-   }
+   }*/
    //send the last bit of data
    //packet_l.seqno++;
    //packet_l.ackno++;
-   packet_t.len = sizeof(packet_t.data);;
+
+
+   //memset(packet_t.data, 0, sizeof(packet_t));
+   packet_t.len = sizeof(packet_t.data);
+   //cout << "len "<< len<<"size of " << " "<< sizeof(packet_t)<<" " << packet_t.data <<" " <<buffer<<endl;
    packet_t.cksum =ck_sum;
-   memcpy ( packet_t.data, buffer, sizeof(packet_t.data) );
-   if (sendto(socket_descriptor, (void *)&  packet_t, sizeof(packet_t), flags, (struct sockaddr *)&destination_address, address_length) == -1) {
-       cout << "Error sending to client, errno = %d (%s) \n" <<endl;
+   memcpy ( packet_t.data, buffer, strlen(buffer)+1 );
+   int result =0;
+   result = sendto(socket_descriptor, (void *)&  packet_t, sizeof(packet_t), flags, destination_address, address_length);
+   //cout << "Result "<<result<<endl;
+   if (  result ==-1) {
+      perror("Error Sending ");
+       //error("Error sending to client\n", errno);
+      // printf("Error no %d %s  \n",errno ,strerror(errno) );
        return -1;
    }
 
-   return 0;
+   return result;
 
 }
